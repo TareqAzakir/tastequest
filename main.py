48df5e29e01f5387d5ffb9a937872620bf480d2e
TareqAzakir <111696000+TareqAzakir@users.noreply.github.com>
2025-06-20 17:51:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+
 from preferences import get_user_preferences
 from feedback import handle_feedback
 from data_handler import load_model_and_data
 
 if __name__ == "__main__":
     print("Hello, welcome to this AI-enhanced recipe suggestion program!")
-    model, le_diet, le_flavor, data = load_model_and_data()
+    model, le_diet, le_flavor, data, recipes = load_model_and_data()
     preferences = get_user_preferences()
-    handle_feedback(preferences, model, le_diet, le_flavor)
+    handle_feedback(preferences, model, le_diet, le_flavor, recipes)
